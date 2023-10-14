96adbf4a053d4c024267421935db9226633153f6
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2023-10-13 18:28:01 -0700

    It doesnt work

---
+++ config/passport.js
@@ -0,0 +1,42 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+
+module.exports = function(passport){
+    passport.use(
+        new LocalStrategy({usernameField: "email", passwordField: 'password', passReqToCallback:false, session:true}, (email,password,done)=> {
+            console.log('Local Strat Works')
+            User.findOne({email: email})
+            .then((user)=>{
+                if(!user){
+                    return done(null, false, {message: 'that email is not registered'})
+                }
+
+                // match pass
+                bcrypt.compare(password, user.password, (err, isMatch)=>{
+                    if(err) throw err;
+
+                    if(isMatch) {
+                        console.log(email + " " + password)
+                        console.log(user)
+                        return(null, user)
+                    }else {
+                        return done(null, false, {message: 'pass incorrect'})
+                    }
+                })
+            })
+        })
+    )
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id)
+    })
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id).then((err, user)=>{
+            done(user, err)
+        })
+    })
+
+}
